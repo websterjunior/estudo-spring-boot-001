3ba0087498d44156885e56663354e971eeae01d4
Webster Junior <webster.junior@mv.com.br>
2021-04-17 23:31:17 -0300

    mapeando arquivos de configuração

--- src/main/java/br/com/websterjunior/api/MeuPrimeiroProjetoApplication.java
+++ src/main/java/br/com/websterjunior/api/MeuPrimeiroProjetoApplication.java
@@ -1,14 +1,27 @@
 package br.com.websterjunior.api;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MeuPrimeiroProjetoApplication {
 
+	@Value("${nome.aplicacao}")
+	private String nomeAplicacao;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
-		System.out.println("INICIALIZANDO O PROJETO");
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner () {
+		return args -> {
+			System.out.println("INICIALIZANDO O PROJETO: " + this.nomeAplicacao);
+		};
+	}
+	
+	
 }
