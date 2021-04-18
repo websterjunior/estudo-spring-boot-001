dc0a7cfbe714bce45dc9fae4ad828260ecad7f4c
Webster Junior <webster.junior@mv.com.br>
2021-04-18 14:32:13 -0300

    definindo profile properties

--- src/test/java/br/com/websterjunior/api/MeuPrimeiroProjetoApplicationTests.java
+++ src/test/java/br/com/websterjunior/api/MeuPrimeiroProjetoApplicationTests.java
@@ -1,13 +1,26 @@
 package br.com.websterjunior.api;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MeuPrimeiroProjetoApplicationTests {
+
+	@Value("${nome.aplicacao}")
+	private String nomeAplicacao;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testCarregarValorProperties() {
+		assertEquals("TESTE", nomeAplicacao);
 	}
 
 }
